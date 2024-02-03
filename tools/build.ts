a2f7c0227b4407d6329aac96f877606329baeb50
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-03 17:56:07 +0100

    Update

--- tools/build.ts
+++ tools/build.ts
@@ -1,15 +1,30 @@
+import { $ } from "bun";
 import { Glob } from "bun";
+import Logger from "@rabbit-company/logger";
+import fs from "node:fs/promises";
+
+Logger.info('Build started');
+
+await $`bunx tailwindcss -i ./tailwind.css -o ./website/css/tailwind.min.css --minify`.quiet();
+Logger.info('TailwindCSS has been built');
+
+await fs.rm('./out', {recursive: true});
+Logger.info('"out" folder has been removed');
 
 let scripts = [];
 
-const glob = new Glob("**.ts");
-for await (const file of glob.scan({cwd: "./website/js"})) {
+const scriptsGlob = new Glob("**.ts");
+for await (const file of scriptsGlob.scan({cwd: "./website/js"})) {
   scripts.push(`./website/js/${file}`);
 }
 
 await Bun.build({
   entrypoints: scripts,
   outdir: "./out/js",
+	target: 'browser',
+	sourcemap: "external",
+	splitting: true,
   minify: true,
   plugins: [],
 });
+Logger.info('Bundler has bundled JS files');
