e3c118a5c8f5ee49571c37a7f421f9bd25d284bd
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-06 23:44:56 +0100

    Update

--- tools/build.ts
+++ tools/build.ts
@@ -9,19 +9,19 @@ Logger.info('Start bulding TailwindCSS...');
 await $`bunx tailwindcss -i ./tailwind.css -o ./website/css/tailwind.min.css --minify`.quiet();
 Logger.info('Bulding TailwindCSS complete');
 
-await fs.rm('./out', {recursive: true});
-Logger.info('"out" folder has been removed');
+await fs.rm('./dist', {recursive: true});
+Logger.info('"dist" folder has been removed');
 
 Logger.info('Start bulding JS files...');
 let scripts = [];
-const scriptsGlob = new Glob("**.ts");
-for await (const file of scriptsGlob.scan({cwd: "./website/js"})) {
+const typeScriptGlob = new Glob("**.ts");
+for await (const file of typeScriptGlob.scan({cwd: "./website/js"})) {
   scripts.push(`./website/js/${file}`);
 }
 
 let scriptBuild = await Bun.build({
   entrypoints: scripts,
-  outdir: "./out/js",
+  outdir: "./dist/js",
 	target: 'browser',
   minify: true,
   plugins: [],
@@ -33,15 +33,29 @@ if(scriptBuild.success){
 }
 
 Logger.info('Start copying HTML files...');
-const htmlsGlob = new Glob("**/**.html");
-for await (const file of htmlsGlob.scan({cwd: "./website"})) {
-	await Bun.write(Bun.file(`./out/${file}`), Bun.file(`./website/${file}`));
+const htmlGlob = new Glob("**/**.html");
+for await (const file of htmlGlob.scan({cwd: "./website"})) {
+	await Bun.write(Bun.file(`./dist/${file}`), Bun.file(`./website/${file}`));
 }
 Logger.info('Copying HTML files complete');
 
 Logger.info('Start copying CSS files...');
 const cssGlob = new Glob("**/**.css");
 for await (const file of cssGlob.scan({cwd: "./website"})) {
-	await Bun.write(Bun.file(`./out/${file}`), Bun.file(`./website/${file}`));
+	await Bun.write(Bun.file(`./dist/${file}`), Bun.file(`./website/${file}`));
 }
 Logger.info('Copying CSS files complete');
+
+Logger.info('Start copying JS files...');
+const scriptGlob = new Glob("**/**.js");
+for await (const file of scriptGlob.scan({cwd: "./website"})) {
+	await Bun.write(Bun.file(`./dist/${file}`), Bun.file(`./website/${file}`));
+}
+Logger.info('Copying JS files complete');
+
+Logger.info('Start copying WASM files...');
+const wasmGlob = new Glob("**/**.wasm");
+for await (const file of wasmGlob.scan({cwd: "./website"})) {
+	await Bun.write(Bun.file(`./dist/${file}`), Bun.file(`./website/${file}`));
+}
+Logger.info('Copying WASM files complete');
