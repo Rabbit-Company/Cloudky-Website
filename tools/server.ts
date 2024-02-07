d371917d9affc43046534c4ffdab6eafbff6b2b0
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-07 17:40:10 +0100

    Update

--- tools/server.ts
+++ tools/server.ts
@@ -8,11 +8,10 @@ Bun.serve({
 	port: 8086,
 	async fetch(req) {
 		try{
-			const filePath = './out' + new URL(req.url).pathname;
+			const filePath = './dist' + new URL(req.url).pathname;
 			const file = Bun.file(filePath);
 			return new Response(file);
 		}catch(err){
-			console.log(err);
 			return new Response(null, { status: 404 });
 		}
 	},
