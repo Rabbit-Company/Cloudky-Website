e43dacb5eb9aaaf7acdf1877c8c5333a8fddf337
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-08 12:54:14 +0100

    Update

--- website/js/icons.ts
+++ website/js/icons.ts
@@ -2,7 +2,9 @@ import Logger from "@rabbit-company/logger";
 
 const iconList: { [key: string]: string } = {
 	'adjustments': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M6 4v4" /><path d="M6 12v8" /><path d="M10 16a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M12 4v10" /><path d="M12 18v2" /><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M18 4v1" /><path d="M18 9v11" />',
-	'adjustments-off': '<path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="6" cy="10" r="2"></circle><path d="M6 6v2"></path><path d="M6 12v8"></path><circle cx="12" cy="16" r="2"></circle><path d="M12 4v4"></path><path d="M12 12v2"></path><path d="M12 18v2"></path><circle cx="18" cy="7" r="2"></circle><path d="M18 4v1"></path><path d="M18 9v5"></path><path d="M18 18v2"></path><path d="M3 3l18 18"></path>'
+	'adjustments-off': '<path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="6" cy="10" r="2"></circle><path d="M6 6v2"></path><path d="M6 12v8"></path><circle cx="12" cy="16" r="2"></circle><path d="M12 4v4"></path><path d="M12 12v2"></path><path d="M12 18v2"></path><circle cx="18" cy="7" r="2"></circle><path d="M18 4v1"></path><path d="M18 9v5"></path><path d="M18 18v2"></path><path d="M3 3l18 18"></path>',
+	'eye': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />',
+	'eye-off': '<path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" /><path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" /><path d="M3 3l18 18" />'
 }
 
 export function getIcon(id: string, color: string = 'secondaryColor', size: number = 5): string{
--- website/js/register.ts
+++ website/js/register.ts
@@ -2,21 +2,43 @@ import PasswordEntropy from '@rabbit-company/password-entropy';
 import { fhide, fshow, isfHidden } from './utils';
 import { setIcon } from './icons';
 
-const passwordInput = document.getElementById("password") as HTMLInputElement;
+const passwordInput = document.getElementById('password') as HTMLInputElement;
 
-const entropyElement = document.getElementById("entropy");
-const serverPickerElement = document.getElementById("server-picker");
+const signInBtnElement = document.getElementById("btn_signin");
+const signUpFormElement = document.getElementById("signup-form");
+const entropyElement = document.getElementById('entropy');
+const serverPickerElement = document.getElementById('server-picker');
+const passwordHiderElement = document.getElementById('password-hider');
 
-passwordInput?.addEventListener("input", () => {
+signInBtnElement?.addEventListener('click', () => {
+	window.location.href = 'index.html';
+});
+
+signUpFormElement?.addEventListener('submit', e => {
+	e.preventDefault();
+});
+
+passwordInput?.addEventListener('input', () => {
 	if(!entropyElement) return;
 
 	let entropy = 100 - (PasswordEntropy.calculate(passwordInput.value));
 	if(entropy <= 1) entropy = 0;
-	entropyElement.style.width = entropy + "%";
+	entropyElement.style.width = entropy + '%';
 });
 
-serverPickerElement?.addEventListener("click", () => {
+serverPickerElement?.addEventListener('click', () => {
 	toggleServerPicker();
+});
+
+passwordHiderElement?.addEventListener('click', () => {
+	if(!passwordInput) return;
+	if(passwordInput.type === "password"){
+		passwordInput.type = "text";
+		setIcon('password-hider', 'eye-off', 'secondaryColor', 5);
+	}else{
+		passwordInput.type = "password";
+		setIcon('password-hider', 'eye', 'secondaryColor', 5);
+	}
 });
 
 function toggleServerPicker(){
